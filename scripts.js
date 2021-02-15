811cfa7eda7b75774007d110d39f3f2728dfa834
yuksel <yukselbey35@gmail.com>
2021-02-14 19:54:39 -0700

    Jquery append method added.

--- scripts.js
+++ scripts.js
@@ -65,6 +65,19 @@
                 $(".changeBlue").css({ background: "white", color: "black" });
             });
 
+            $( "li" ).hover(
+                function() {
+                  $( this ).append( $( "<span> ***</span>" ) );
+                }, function() {
+                  $( this ).find( "span" ).last().remove();
+                }
+              );
+               
+              $( "li.fade" ).hover(function() {
+                $( this ).fadeOut( 100 );
+                $( this ).fadeIn( 500 );
+              });
+
 
         });
     
